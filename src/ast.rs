9ef01343fc787748389c8789171f2438f42f74df
iho <iho@users.noreply.github.com>
2024-07-01 03:55:00 +0300

    I am retarded

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,8 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Expr {
-    Universe(i64),
-    Lam(String, i64, Box<Expr>),
-    Pi(String, i64, Box<Expr>),
-    App(Box<Expr>, Box<Expr>),
-    Var(String, i64),
-}
-pub fn skip_first_char(s: &str) -> String {
-    s.chars().skip(1).collect()
+    Star,
+    Var(String),
+    AngleBracket(String),
+    SquareBrackets(String, Box<Expr>),
+    RoundBrackets(String, Box<Expr>, Option<Box<Expr>>),
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,14 @@ fn print(s: &str) {
 
 
 fn main() {
-    print("*11");
-    print("Πx11.*11");
-    print("λy4.*11");
+    print("[x:alpha]false");
+    print("not(for([z,elt(s)]not(<z>p)))");
+    print("for([z,elt(s)]imp(<z>p,for([z',elt(s)]imp(<z'>p,eq(z,z')))))");
+    // print("* nat : TYPE := PN");
+    // print("*   1 : nat  := EB");
+    // print("*   x : nat  := ---");
+
+    // print("x * successor : nat := PN ");
+    // print("* 2         : nat := successor(1)");
+    // print("* 3         : nat := successor(2)");
 }
