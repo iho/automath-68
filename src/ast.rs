f6d8780cedd8f1a7b7da77ca770bd87122c7b8be
5HT <maxim@synrc.com>
2024-07-03 01:44:01 +0300

    cleanup

--- src/ast.rs
+++ src/ast.rs
@@ -2,7 +2,7 @@
 pub enum Henk {
     U(i64),
     Var(String),
-    App(Box<Henk>, Box<Henk>),
+    Pi(String, Box<Henk>, Box<Henk>),
     Lam(String, Box<Henk>, Box<Henk>),
-    Pi(String, Box<Henk>, Box<Henk>)
+    App(Box<Henk>, Box<Henk>),
 }
