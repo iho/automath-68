df8a84b48aeef9bef1517548fceff598fc77ac00
5HT <maxim@synrc.com>
2024-07-02 21:38:53 +0300

    term

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ pub mod ast;
 fn print(s: &str) { println!("{:?}", grammar::ExprParser::new().parse(s).unwrap()); }
 
 fn main() {
-   print("[A:*][B:[_:A]*][x:A](B x)");
-   print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L) C H (T L C N)");
+   print("[A:*][B:[_:A]*][x:A](B(x))");
+   print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L)(C H (T L C N))");
 }
