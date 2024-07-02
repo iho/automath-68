d2607d6857b98c71f935030e435a97318b78b3d9
iho <iho@users.noreply.github.com>
2024-07-03 02:42:00 +0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub grammar);
 pub mod ast;
-fn print(s: &str) { println!("{:?}", grammar::ExprParser::new().parse(s).unwrap()); }
-
+fn print(s: &str) {
+    let res = grammar::ExprParser::new().parse(s).unwrap(); 
+    println!("{:?}", res);
+    println!("{:?}", res.type_check());
+}
 fn main() {
    print("[A:*][B:A] (A B)");
    print("(A:*)(H:A)(T:[L:*][C:A->(L->L)][N:L]L)(L:*)(C:A->(L->L))(N:L) C H (T L C N)");
