a75f018a371eb773f006283f569205e81cbc1d4d
5HT <maxim@synrc.com>
2024-07-03 21:32:05 +0300

    Milestone 1. List typechecking passed.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub grammar);
 pub mod ast;
-fn print(s: &str) { println!("{:?}", grammar::ExprParser::new().parse(s).unwrap()); }
-
+fn print(s: &str) {
+    let res = grammar::ExprParser::new().parse(s).unwrap(); 
+    println!("{:?}", res);
+    println!("{:?}", res.type_check());
+}
 fn main() {
-   print("[A:*134][B:[_:A]*][x:A](B(x))");
+   print("(x:*) x");
+   print("(x:*11) x");
    print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L)(C H (T L C N))");
+   print("(A: *) (Head: A) (Tail: [List: *] [Cons: [_:A] [_: List] List] [Nil: List] List) (List: *) (Cons: [_:A] [_: List] List) (Nil: List) (Cons Head (Tail List Cons Nil))");
 }
