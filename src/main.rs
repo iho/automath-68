bfa8a5c7375ca896a975fcdc44335315fb27b4f5
iho <iho@users.noreply.github.com>
2024-07-15 16:24:30 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,9 @@ fn main() {
     //    print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L)(C H (T L C N))");
     //    print("(A: *) (Head: A) (Tail: [List: *] [Cons: [_:A] [_: List] List] [Nil: List] List) (List: *) (Cons: [_:A] [_: List] List) (Nil: List) (Cons Head (Tail List Cons Nil))");
     // ∀(Nat : *) → ∀(Succ : Nat → Nat) → ∀(Zero : Nat) → Nat
-    print("(Nat: *) (Succ: [_:Nat] Nat) (Zero: Nat) Zero");
+    // print("(Nat: *) (Succ: [_:Nat] Nat) (Zero: Nat) Zero");
     // λ(pred : ∀(Nat : *) → ∀(Succ : Nat → Nat) → ∀(Zero : Nat) → Nat) → λ(Nat : *) → λ(Succ : Nat → Nat) → λ(Zero : Nat) →
     //  Succ (pred Nat Succ Zero)
-    print("(pred: (Nat: *)  (Succ: [_:Nat] Nat)  (Zero: Nat)  Nat)  (Nat: *) (Succ: [_:Nat] Nat)   (Zero: Nat)   
-        Succ (pred Nat Succ Zero)");
+    print("(pred: (Nat: *)  (Succ: [_:Nat] Nat)  (Zero: Nat)  Nat)  (Nat: *) (Succ: [_:Nat] Nat)   (Zero: Nat)   Succ (pred Nat Succ Zero)");
 
 }
