7596e42f6e7997e47cecbeba8479c1b0505cafe7
5HT <maxim@synrc.com>
2024-07-15 16:40:00 +0300

    Nat Succ

--- src/ast.rs
+++ src/ast.rs
@@ -20,22 +20,20 @@ impl Display for Henk {
             Variable(ref str) => write!(f, "{}", str),
             Application(ref left, ref right) => write!(f, "({} {})", left, right),
             Lambda(ref bound, ref ty, ref inner) => write!(f, "(\\{}: {}. {})", bound, ty, inner),
-            Forall(ref bound, ref left, ref right) => {
-                write!(f, "({}: {}) -> {}", bound, left, right)
-            }
+            Forall(ref bound, ref left, ref right) => { write!(f, "({}: {}) -> {}", bound, left, right) }
             Universe(i) => write!(f, "Universe {}", i),
         }
     }
 }
 
 impl Henk {
+
     pub fn type_check(self) -> Result<Henk, String> {
         debug!("Start type checking.");
-        let mut context: HashMap<String, Henk> = HashMap::new();
-        self.type_check_with_context(&mut context)
+        self.type_check_with_context(HashMap::new())
     }
 
-    pub fn type_check_with_context(self, context: &mut HashMap<String, Henk>) -> Result<Henk, String> {
+    pub fn type_check_with_context(self, context: HashMap<String, Henk>) -> Result<Henk, String> {
         println!("Type checking: {}", self);
         println!("Context: {:?}", context);
         match self {
@@ -45,51 +43,34 @@ impl Henk {
                 None => Err(format!("Cannot find variable {}.", &v)),
             },
             Henk::Application(left, right) => {
-                let left_ty = left.type_check_with_context(context)?;
+                let left_ty = left.type_check_with_context(context.clone())?;
                 match left_ty.whnf() {
-                    Henk::Lambda(bound, ty_in, ty_out) => {
-                        let right_ty = right.clone().type_check_with_context(context)?;
-                        if right_ty.beta_eq(&ty_in) {
-                            Ok(ty_out.substitute(&bound, &right))
-                        } else {
-                            Err(format!(
-                                "Expected something of type {}, found that of type {}.",
-                                ty_in, right_ty
-                            ))
-                        }
-                    }
-                    left_ty => Err(format!("Expected lambda, found value of type {}.", left_ty)),
+                    Henk::Forall(bound, ty_in, ty_out) => {
+                        let right_ty = right.clone().type_check_with_context(context.clone())?;
+                        if right_ty.beta_eq(&ty_in) { Ok(ty_out.substitute(&bound, &right)) } else
+                        { Err(format!("Expected something of type {}, found that of type {}.", ty_in, right_ty), ) }
+                    }  left_ty => { Err(format!("Expected lambda, found value of type {}.", left_ty)) }
                 }
             }
             Henk::Lambda(bound, ty, inner) => {
-                ty.clone().type_check_with_context(context)?;
-                // let mut new_context = context;
-                context.insert(bound.clone(), *ty.clone());
-                let inner_type = inner.type_check_with_context(context)?;
+                ty.clone().type_check_with_context(context.clone())?;
+                let mut new_context = context;
+                new_context.insert(bound.clone(), *ty.clone());
+                let inner_type = inner.type_check_with_context(new_context)?;
                 Ok(Henk::Forall(bound, ty, Box::new(inner_type)))
             }
             Henk::Forall(bound, left, right) => {
-                let left_sort = left
-                    .clone()
-                    .type_check_with_context(context)
-                    .map(Henk::whnf)?;
-                context.insert(bound.clone(), *left.clone());
-                let right_kind = right
-                    .clone()
-                    .type_check_with_context(context)
-                    .map(Henk::whnf)?;
+                let left_sort = left.clone().type_check_with_context(context.clone()).map(Henk::whnf)?;
+                let mut new_context = context;
+                new_context.insert(bound.clone(), *left.clone());
+                let right_kind = right.clone().type_check_with_context(new_context).map(Henk::whnf)?;
                 Ok(Henk::Forall(bound.clone(), left.clone(), right))
             }
         }
     }
 
-
     pub fn free_vars(&self) -> HashSet<&String> {
-        fn closure_vars<'a>(
-            bound: &'a String,
-            ty: &'a Box<Henk>,
-            inner: &'a Box<Henk>,
-        ) -> HashSet<&'a String> {
+        fn  closure_vars<'a>(bound: &'a String, ty: &'a Box<Henk>, inner: &'a Box<Henk>) -> HashSet<&'a String> {
             let mut tmp = inner.free_vars();
             tmp.remove(bound);
             tmp.union(&ty.free_vars()).cloned().collect()
@@ -97,23 +78,10 @@ impl Henk {
         let mut set = HashSet::new();
         match *self {
             Henk::Universe(v) => {}
-            Henk::Variable(ref v) => {
-                set = HashSet::new();
-                set.insert(v);
-            }
-            Henk::Application(ref left, ref right) => {
-                set = left
-                    .free_vars()
-                    .union(&right.free_vars())
-                    .cloned()
-                    .collect()
-            }
-            Henk::Lambda(ref bound, ref ty, ref inner) => {
-                closure_vars(bound, ty, inner);
-            }
-            Henk::Forall(ref bound, ref left, ref right) => {
-                closure_vars(bound, left, right);
-            }
+            Henk::Variable(ref v) => { set = HashSet::new(); set.insert(v); }
+            Henk::Application(ref left, ref right) => { set = left.free_vars().union(&right.free_vars()).cloned().collect() }
+            Henk::Lambda(ref bound, ref ty, ref inner) => { closure_vars(bound, ty, inner); }
+            Henk::Forall(ref bound, ref left, ref right) => { closure_vars(bound, left, right); }
         }
         set
     }
@@ -121,50 +89,21 @@ impl Henk {
     pub fn substitute(self, from: &String, to: &Henk) -> Henk {
         match self {
             Henk::Universe(v) => Henk::Universe(v),
-            Henk::Variable(v) => {
-                if v == *from {
-                    to.clone()
-                } else {
-                    Henk::Variable(v)
-                }
-            }
-            Henk::Application(left, right) => Henk::Application(
-                Box::new(left.substitute(from, to)),
-                Box::new(right.substitute(from, to)),
-            ),
+            Henk::Variable(v) => { if v == *from { to.clone() } else { Henk::Variable(v) } }
+            Henk::Application(left, right) => { Henk::Application(Box::new(left.substitute(from, to)), Box::new(right.substitute(from, to))) }
             Henk::Lambda(ref bound, ref ty, ref inner) => {
-                if bound == from {
-                    Henk::Lambda(
-                        bound.clone(),
-                        Box::new(ty.clone().substitute(from, to)),
-                        inner.clone(),
-                    )
-                } else {
-                    if !to.free_vars().contains(bound) {
-                        Henk::Lambda(
-                            bound.clone(),
-                            Box::new(ty.clone().substitute(from, to)),
-                            Box::new(inner.clone().substitute(from, to)),
-                        )
-                    } else {
+                if bound == from { Henk::Lambda(bound.clone(),Box::new(ty.clone().substitute(from, to)),inner.clone(),) }
+                else {
+                    if !to.free_vars().contains(bound) { Henk::Lambda(bound.clone(),Box::new(ty.clone().substitute(from, to)),Box::new(inner.clone().substitute(from, to)),) }
+                    else {
                         let mut unused: String = bound.clone();
                         unused.push_str("'");
                         loop {
-                            let used: HashSet<&String> =
-                                inner.free_vars().union(&to.free_vars()).cloned().collect();
-                            if used.contains(&unused) {
-                                unused.push_str("'")
-                            } else {
-                                let renamed = Henk::Lambda(
-                                    unused.clone(),
-                                    Box::new(
-                                        ty.clone()
-                                            .substitute(bound, &Henk::Variable(unused.clone())),
-                                    ),
-                                    Box::new(
-                                        inner.clone().substitute(bound, &&Henk::Variable(unused)),
-                                    ),
-                                );
+                            let used: HashSet<&String> = inner.free_vars().union(&to.free_vars()).cloned().collect();
+                            if used.contains(&unused) { unused.push_str("'") }
+                            else {
+                                let renamed = Henk::Lambda(unused.clone(), Box::new(ty.clone().substitute(bound, &Henk::Variable(unused.clone()), )),
+                                    Box::new(inner.clone().substitute(bound, &&Henk::Variable(unused)), ), );
                                 return renamed.substitute(from, to);
                             }
                         }
@@ -172,38 +111,16 @@ impl Henk {
                 }
             }
             Henk::Forall(ref bound, ref left, ref right) => {
-                if bound == from {
-                    Henk::Forall(
-                        bound.clone(),
-                        Box::new(left.clone().substitute(from, to)),
-                        right.clone(),
-                    )
-                } else {
-                    if !to.free_vars().contains(bound) {
-                        Henk::Forall(
-                            bound.clone(),
-                            Box::new(left.clone().substitute(from, to)),
-                            Box::new(right.clone().substitute(from, to)),
-                        )
-                    } else {
+                if bound == from { Henk::Forall(bound.clone(),Box::new(left.clone().substitute(from, to)),right.clone(),) }
+                else {
+                    if !to.free_vars().contains(bound) { Henk::Forall(bound.clone(), Box::new(left.clone().substitute(from, to)), Box::new(right.clone().substitute(from, to)),) }
+                    else {
                         let mut unused: String = bound.clone();
                         unused.push_str("'");
                         loop {
-                            let used: HashSet<&String> =
-                                right.free_vars().union(&to.free_vars()).cloned().collect();
-                            if used.contains(&unused) {
-                                unused.push_str("'")
-                            } else {
-                                let renamed = Henk::Forall(
-                                    unused.clone(),
-                                    Box::new(
-                                        left.clone()
-                                            .substitute(bound, &&&Henk::Variable(unused.clone())),
-                                    ),
-                                    Box::new(
-                                        right.clone().substitute(bound, &&Henk::Variable(unused)),
-                                    ),
-                                );
+                            let used: HashSet<&String> = right.free_vars().union(&to.free_vars()).cloned().collect();
+                            if used.contains(&unused) { unused.push_str("'") } else {
+                                let renamed = Henk::Forall(unused.clone(), Box::new(left.clone().substitute(bound,&&&Henk::Variable(unused.clone()),)), Box::new(right.clone().substitute(bound, &&Henk::Variable(unused)),), );
                                 return renamed.substitute(from, to);
                             }
                         }
@@ -223,12 +140,10 @@ impl Henk {
                 }
                 (Henk::Lambda(ref from, _, ref inner), ref stack) if !stack.is_empty() => {
                     let mut new_stack: Vec<Henk> = (*stack).into();
-                    let right = new_stack.last().unwrap();
+                    let right = new_stack.pop().unwrap();
                     spine(inner.clone().substitute(&from, &right), &new_stack)
                 }
-                (leftmost, _) => stack.iter().fold(leftmost, |l, r| {
-                    Henk::Application(Box::new(l), Box::new(r.clone()))
-                }),
+                (leftmost, _) => stack.iter().fold(leftmost, |l, r| Henk::Application(Box::new(l), Box::new(r.clone()))),
             }
         }
         spine(self, &[])
@@ -243,28 +158,18 @@ impl Henk {
                     spine(*left, &new_stack)
                 }
                 (Henk::Lambda(ref from, ref ty, ref inner), ref stack) if stack.is_empty() => {
-                    Henk::Lambda(
-                        from.clone(),
-                        Box::new(ty.clone().nf()),
-                        Box::new(inner.clone().nf()),
-                    )
+                    Henk::Lambda(from.clone(), Box::new(ty.clone().nf()), Box::new(inner.clone().nf()), )
                 }
                 (Henk::Lambda(ref from, _, ref inner), ref stack) => {
                     let mut new_stack: Vec<Henk> = (*stack).into();
-                    let right = new_stack.last().unwrap();
+                    let right = new_stack.pop().unwrap();
                     spine(inner.clone().substitute(&from, &right), &new_stack)
                 }
                 (Henk::Forall(ref bound, ref left, ref inner), ref stack) => stack.iter().fold(
-                    Henk::Forall(
-                        bound.clone(),
-                        Box::new(left.clone().nf()),
-                        Box::new(inner.clone().nf()),
-                    ),
-                    |l, r| Henk::Application(Box::new(l), Box::new(r.clone().nf())),
+                    Henk::Forall(bound.clone(), Box::new(left.clone().nf()), Box::new(inner.clone().nf()), ),
+                    |l, r| Henk::Application(Box::new(l), Box::new(r.clone().nf()))
                 ),
-                (leftmost, _) => stack.iter().fold(leftmost, |l, r| {
-                    Henk::Application(Box::new(l), Box::new(r.clone().nf()))
-                }),
+                (leftmost, _) => stack.iter().fold(leftmost, |l, r| Henk::Application(Box::new(l), Box::new(r.clone().nf())) ),
             }
         }
         spine(self, &[])
@@ -274,32 +179,9 @@ impl Henk {
         match (self, another) {
             (&Henk::Universe(v1), &Henk::Universe(v2)) => v1 == v2,
             (&Henk::Variable(ref v1), &Henk::Variable(ref v2)) => v1 == v2,
-            (
-                &Henk::Application(ref left1, ref right1),
-                &Henk::Application(ref left2, ref right2),
-            ) => left1.alpha_eq(&left2) && right1.alpha_eq(&right2),
-            (
-                &Henk::Lambda(ref bound1, ref ty1, ref inner1),
-                &Henk::Lambda(ref bound2, ref ty2, ref inner2),
-            ) => {
-                ty1.alpha_eq(ty2)
-                    && inner1.alpha_eq(
-                        &inner2
-                            .clone()
-                            .substitute(&bound2, &&&Henk::Variable(bound1.clone())),
-                    )
-            }
-            (
-                &Henk::Forall(ref bound1, ref left1, ref right1),
-                &Henk::Forall(ref bound2, ref left2, ref right2),
-            ) => {
-                left1.alpha_eq(left2)
-                    && right1.alpha_eq(
-                        &right2
-                            .clone()
-                            .substitute(&bound2, &Henk::Variable(bound1.clone())),
-                    )
-            }
+            (&Henk::Application(ref left1, ref right1),&Henk::Application(ref left2, ref right2),) => left1.alpha_eq(&left2) && right1.alpha_eq(&right2),
+            (&Henk::Lambda(ref bound1, ref ty1, ref inner1),&Henk::Lambda(ref bound2, ref ty2, ref inner2),) => { ty1.alpha_eq(ty2) && inner1.alpha_eq(&inner2.clone().substitute(&bound2, &&&Henk::Variable(bound1.clone())),) }
+            (&Henk::Forall(ref bound1, ref left1, ref right1),&Henk::Forall(ref bound2, ref left2, ref right2),) => { left1.alpha_eq(left2) && right1.alpha_eq(&right2.clone().substitute(&bound2, &Henk::Variable(bound1.clone())),) }
             _ => false,
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,10 @@ fn print(s: &str) {
     println!("{:?}", res.type_check());
 }
 fn main() {
-    //    print("(x:*) x");
-    //    print("(x:*11) x");
-    //    print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L)(C H (T L C N))");
-    //    print("(A: *) (Head: A) (Tail: [List: *] [Cons: [_:A] [_: List] List] [Nil: List] List) (List: *) (Cons: [_:A] [_: List] List) (Nil: List) (Cons Head (Tail List Cons Nil))");
-    // ∀(Nat : *) → ∀(Succ : Nat → Nat) → ∀(Zero : Nat) → Nat
-    // print("(Nat: *) (Succ: [_:Nat] Nat) (Zero: Nat) Zero");
-    // λ(pred : ∀(Nat : *) → ∀(Succ : Nat → Nat) → ∀(Zero : Nat) → Nat) → λ(Nat : *) → λ(Succ : Nat → Nat) → λ(Zero : Nat) →
-    //  Succ (pred Nat Succ Zero)
-    print("(pred: (Nat: *)  (Succ: [_:Nat] Nat)  (Zero: Nat)  Nat)  (Nat: *) (Succ: [_:Nat] Nat)   (Zero: Nat)   Succ (pred Nat Succ Zero)");
-
+   print("(x:*) x");
+   print("(x:*11) x");
+   print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L)(C H (T L C N))");
+   print("(A: *) (Head: A) (Tail: [List: *] [Cons: [_:A] [_: List] List] [Nil: List] List) (List: *) (Cons: [_:A] [_: List] List) (Nil: List) (Cons Head (Tail List Cons Nil))");
+   print("[Nat : *] [Succ : [_:Nat] Nat] [Zero : Nat] Nat");
+   print("(pred : [Nat : *] [Succ : [_ : Nat] Nat] [Zero : Nat] Nat) (Nat : *) (Succ : [_: Nat] Nat) (Zero : Nat) (Succ (pred Nat Succ Zero))");
 }
