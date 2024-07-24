ea46dd7a491d5a505f075022b908aa9b7668373f
5HT <maxim@synrc.com>
2024-07-24 16:24:50 +0300

    equ

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,8 @@ fn print(s: &str) {
     println!("Type: {:?}", res.type_check());
     println!("");
 }
+
 fn main() {
-
-/*
-  λ (x : ∀(Nat : *) → ∀ (Succ : Nat → Nat) → ∀ (Zero : Nat) → Nat)
-→ x (∀ (Nat : *) → ∀ (Succ : Nat → Nat) → ∀ (Zero : Nat) → Nat)
-    ( λ (pred : ∀(Nat : *) → ∀ (Succ : Nat → Nat) → ∀ (Zero : Nat) → Nat)
-    → λ (Nat : *)
-    → λ (Succ : Nat → Nat)
-    → λ (Zero : Nat) → Succ (pred Nat Succ Zero))
-*/
-
    print("(x:*) x");
    print("(x:*11) x");
    print("(A:*)(H:A)(T:[L:*][C:[_:A][_:L]L][N:L]L)(L:*)(C:[_:A][_:L]L)(N:L)(C H (T L C N))");
@@ -27,5 +18,6 @@ fn main() {
    print("(pred : [Nat : *] [Succ : [_ : Nat] Nat] [Zero : Nat] Nat) (Nat : *) (Succ : [_: Nat] Nat) (Zero : Nat) (Succ (pred Nat Succ Zero))");
    print("[Nat:*] [Succ: [_:Nat] Nat] [Zero: Nat] Nat");
    print("(x: [Nat:*] [Succ: [_:Nat] Nat] [Zero: Nat] Nat) (x ([Nat:*][Succ:[_:Nat]Nat][Zero:Nat]Nat) ((pred: [Nat:*] [Succ: [_:Nat] Nat] [Zero:Nat] Nat) (Nat2:*) (Succ2:[_:Nat2] Nat2) (Zero2: Nat2) (Succ2 (pred Nat2 Succ2 Zero2))))");
-
+   print("(A: *) (x: A) (y: A) [Equ: [a:A] [b:A] *] [Refl: [z: A] Equ z z] (Equ x y)");
+   print("(A: *) (x: A) (Equ: [_:A] [_:A] *) (Refl: [z: A] (Equ z z)) (Refl x)");
 }
